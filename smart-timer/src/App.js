e22b521597deb6dcd245593fd3582e16c5a88859
sezgink <sezginkutlu00@gmail.com>
2022-08-02 00:14:54 +0300

    Simple timer

--- smart-timer/src/App.js
+++ smart-timer/src/App.js
@@ -7,19 +7,40 @@ import {useState} from 'react';
 
 function App() {
   let [isCounting,setIsCounting] = useState(false);
+  // let intervalID;
   function onStartToggle(){
+    if(isCounting)
+    {
+      stopCounting();
+      
+    } else {
+      startCounting();
+    }
     setIsCounting(!isCounting);
+  }
+  let [currentCount,setCurrentCount] = useState(0);
+  let [intervalID,setIntervalID] = useState(0);
+  function tickTime(){
+    // let newCount = currentCount+1;
+     setCurrentCount(currentCount=>currentCount+1);
+  }
+  function startCounting() {
+    let newIntervalID = setInterval(tickTime, 1000);
+    setIntervalID(newIntervalID);
+  }
+  function stopCounting(){
+    clearInterval(intervalID);
+    
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
         <StartToggle isCounting={isCounting} onToggle={onStartToggle}/>
-        
+        {currentCount}
+        </div>
         
       </header>
     </div>
--- smart-timer/src/startToggle.js
+++ smart-timer/src/startToggle.js
@@ -3,10 +3,11 @@
 function StartToggle(props){
 
     if(props.isCounting===true){
-        return(<button className="positiveButton" onClick={props.onToggle}>Start</button>);
+        return(<button className="negativeButton" onClick={props.onToggle}>Stop</button>) ;
+        
     }
-
-    return(<button className="negativeButton" onClick={props.onToggle}>Stop</button>) ;
+    return(<button className="positiveButton" onClick={props.onToggle}>Start</button>);
+    
   }
 
   export default StartToggle;
