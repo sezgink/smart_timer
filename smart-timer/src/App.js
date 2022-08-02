5ae749286097178733b982db588739b19ef21563
sezgink <sezginkutlu00@gmail.com>
2022-08-03 00:31:05 +0300

    Show past interval lengths

--- smart-timer/src/App.js
+++ smart-timer/src/App.js
@@ -20,9 +20,12 @@ function App() {
   }
   let [currentCount,setCurrentCount] = useState(0);
   let [intervalID,setIntervalID] = useState(0);
+  let [pastIntervals,setPastIntervals] = useState([Number]);
+
   function tickTime(){
     // let newCount = currentCount+1;
      setCurrentCount(currentCount=>currentCount+1);
+     
   }
   function startCounting() {
     let newIntervalID = setInterval(tickTime, 1000);
@@ -30,16 +33,28 @@ function App() {
   }
   function stopCounting(){
     clearInterval(intervalID);
+    setPastIntervals(pastIntervals=>[...pastIntervals,currentCount]);
     
   }
+  const pastIntervalList = pastIntervals.map((pastInterval)=>
+    <div key={pastInterval}>
+    <p>{pastInterval}</p>
+    </div>
+  );
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {//<img src={logo} className="App-logo" alt="logo" /> 
+        }
         <div>
+        {pastIntervalList}
         <StartToggle isCounting={isCounting} onToggle={onStartToggle}/>
+
         {currentCount}
+        {'\n'}
+        {new Date(currentCount*1000).toISOString().substring(11, 19)}
+
         </div>
         
       </header>
