7abd9acfeccbcb7ce5051838d8620cd49ab6aa35
sezgink <sezginkutlu00@gmail.com>
2022-11-11 22:01:44 +0300

    Posting signup data

--- smart-timer/src/signup.js
+++ smart-timer/src/signup.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Grid } from '@mui/material';
 
-const signupUrl = "http://localhost:5445/login"
+const signupUrl = "http://localhost:9443/login"
 const SignupForm = () => (
   <div style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
     <Grid container spacing={2} style={{alignItems:'center',justifyContent:'center',direction:'column'}}>
@@ -24,11 +24,21 @@ const SignupForm = () => (
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-        fetch("")
+        console.log("Submiting");
+        const fetchOptions = {
+          headers: {
+            // 'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          body: JSON.stringify(values, null, 2)
+        }
+        console.log(JSON.stringify(values, null, 2));
+        fetch(signupUrl,fetchOptions).then((res)=>{
+          console.log(res);
+        }).catch((err)=>{
+          console.log(err);
+        });
       }}
     >
       {({ isSubmitting }) => (
@@ -38,7 +48,8 @@ const SignupForm = () => (
           <ErrorMessage name="email" component="div" />
           <Field type="password" name="password" />
           <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
+          {/* <button type="submit" disabled={isSubmitting}> */}
+          <button type="submit">
             Submit
           </button>
         </Form>
