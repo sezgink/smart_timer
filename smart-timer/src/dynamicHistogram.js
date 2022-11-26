486bdb861239be10f6469723f662f20d6f8df5e0
sezgink <sezginkutlu00@gmail.com>
2022-11-26 14:17:48 +0300

    Cleaner histogram

--- smart-timer/src/dynamicHistogram.js
+++ smart-timer/src/dynamicHistogram.js
@@ -10,13 +10,7 @@ const DynamicHistogram = (props)=>{
         setNormalizationCo(1/max);
     },[props.datas]);
 
-    useEffect(()=>{
-        
-    });
-
     const increaseMultImmediately = (lastVal) =>{
-        // setHistogramMult(histogramMult=>histogramMult+0.1);
-        // setHistogramMult(1);
         setTimeout(()=>{
           setHistogramMult(1);
         },1);  
@@ -26,14 +20,13 @@ const DynamicHistogram = (props)=>{
         <div>
             <div className='workChartDiv'>
                 {props.datas.map((item,index)=>{
-                    return(<div className='workChartBox' key={index} style={{width:30,height:200*item*histogramMult*normalizationCo,transition: 'height 0.7s ease-out'}}/>)
-                })}
-
-
-            </div>
-            <div className='workChartBottom'>
-            {props.datas.map((item,index)=>{
-                    return(<div className='workChartEmptyBox' key={index} style={{width:30,height:0}}><h3>{index}</h3></div>)
+                    // return(<div className='workChartBox' key={index} style={{width:30,height:200*item*histogramMult*normalizationCo,transition: 'height 0.7s ease-out'}}/>)
+                    return(
+                    <div className='workChartBox' key={index} style={{width:30}}>
+                        <div className="workChartColoredBox" style={{width:"100%",height:200*item*histogramMult*normalizationCo,transition: 'height 0.7s ease-out'}}/>
+                        <div className='workChartEmptyBox' key={index} style={{width:30,height:0}}><h3>{index}</h3></div>
+                    </div>
+                    )
                 })}
             </div>
         </div>
