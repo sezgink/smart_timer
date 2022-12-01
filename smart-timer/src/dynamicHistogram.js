e14ed0429a67365602d48dc59b2840a486706903
sezgink <sezginkutlu00@gmail.com>
2022-12-01 22:14:52 +0300

    Charts closed until a meaningful data come

--- smart-timer/src/dynamicHistogram.js
+++ smart-timer/src/dynamicHistogram.js
@@ -63,9 +63,9 @@ const DynamicHistogram2 = (props)=>{
                 {props.datas.map((item,index)=>{
                     return(
                     <div className='workChartBox' key={index} style={{width:30}}>
-                        <div className='workChartEmptyBox' style={{width:30,height:50,marginBottom:-15,textAlign:"center"}}><h3>{round2digits(item.value)}</h3></div>
+                        <div className='workChartEmptyBox' style={{width:30,height:50,marginBottom:-15,textAlign:"center",justifyContent:"center",alignItems:"center",display:"flex"}}><h4>{round2digits(item.value)}h</h4></div>
                         <div className="workChartColoredBox" style={{width:"100%",height:200*item.value*histogramMult*normalizationCo,transition: 'height 0.7s ease-out'}}/>
-                        <div className='workChartEmptyBox'  style={{width:30,height:0,textAlign:"center"}}><h3>{item.label}</h3></div>
+                        <div className='workChartEmptyBox'  style={{width:30,height:30,textAlign:"center",justifyContent:"center",alignItems:"center",display:"flex"}}><h4>{item.label}</h4></div>
                     </div>
                     )
                 })}
--- smart-timer/src/workChart.js
+++ smart-timer/src/workChart.js
@@ -35,7 +35,7 @@ function getMinDate(datesArr){
   });
 
 
-  console.log(minDate);
+  // console.log(minDate);
   return minDate;
 }
 
@@ -60,9 +60,14 @@ function BasicDatePicker(props) {
         setValue(newVal);
     }
 
+    function getDaysInMonth(year, month) {
+      return new Date(year, month+1, 0).getDate();
+    }
+      
+
     function GetNewIntervals(){
         //fetch with value
-        console.log(value);
+        // console.log(value);
 
         const fetchOptions = {
             headers: {
@@ -79,7 +84,7 @@ function BasicDatePicker(props) {
           const beginDate = value[0];
           // const endDate = value[1].setHours(23,59,59);
           const endDate = dayjs(value[1]).endOf('day').toDate();
-          console.log({beginDate,endDate});
+          // console.log({beginDate,endDate});
           
           fetch(url2fetch+new URLSearchParams({beginDate ,endDate}),fetchOptions).then((res)=>{
             // console.log("Response came");
@@ -93,7 +98,10 @@ function BasicDatePicker(props) {
 
                 // alert("Wrong email or password!");
               }
-              props.onIntervalsCome(jres);
+              const daysInMonth = getDaysInMonth(beginDate.getFullYear(),beginDate.getMonth());
+              // const firstDay = beginDate.getDate();
+              const firstDay = beginDate;
+              props.onIntervalsCome(jres,{firstDay,daysInMonth});
               
               
             }).catch((err)=>{
@@ -136,9 +144,11 @@ function BasicDatePicker(props) {
   
 
 const WorkChart = (props)=>{
-  let [histogramValues,setHistogramValues] = useState([0]);
+  // let [histogramValues,setHistogramValues] = useState([0]);
+  let [histogramValues,setHistogramValues] = useState([]);
   let [intervalsData,setIntervalsData] = useState([]);
-    function onIntervalsCome(data){
+  let [labelData,setLabelData] = useState({});
+    function onIntervalsCome(data,beginInfo){
       // console.log("data"+ data.toString());
       const intervalsBetween = data.intervalsBetween;
       const timesPerDay = intervalsBetween.map((dailyData)=>{
@@ -147,15 +157,39 @@ const WorkChart = (props)=>{
         },0)
         return dailySum;
       });
-      
-      console.log(timesPerDay);
+      // console.log(timesPerDay);
+      setLabelData({firstDay :beginInfo.firstDay, daysInMonth:beginInfo.daysInMonth});
       setHistogramValues(timesPerDay);
       setIntervalsData(intervalsBetween);
+      
     }
 
     const valuesToValueAndLabels = (valuesArray)=>valuesArray.map((value,index)=>{
-      return {value, label:index}
+      // return {value, label:index}
+      
+      // console.log(labelData.firstDay);
+      let month = 0;
+      let rawDay = 0;
+
+      if(labelData.firstDay){
+        rawDay = labelData.firstDay.getDate()+index;
+        month = labelData.firstDay.getMonth();
+        // console.log(month);
+        if(rawDay>labelData.daysInMonth){
+          rawDay = rawDay%labelData.daysInMonth;
+          month+=1;
+        }
+      }
+      
+      return {value, label: (labelData.firstDay)?(month+"."+rawDay):null};
     });
+
+    const checkForShow = ()=>{
+      // const checkValues = histogramValues.reduce((rv,x)=>{
+      //   return (x != 0)||rv;
+      // });
+      return (histogramValues.length>1)||histogramValues[0]>0;
+    }
 
     return (<div style={{display:'flex',flexDirection:'column'}}>
       <div style={{alignItems:'center',width:"100%",justifyContent:"center",marginBottom:20,marginTop:10}}>
@@ -164,10 +198,14 @@ const WorkChart = (props)=>{
         
         {/* <DynamicHistogram2 datas={[{value : 1, label: "Mon"},{value : 0.5, label: "Tue"},{value : 0.2, label: "Wed"},{value : 0.6, label: "Thu"},{value : 0.5, label: "Fri"}]}/> */}
         {/* <DynamicHistogram datas={histogramValues}/> */}
-        <DynamicHistogram2 datas={valuesToValueAndLabels(histogramValues)}/>
-          <div style={{display:'flex', justifyContent:'center',alignItems:'center',flexDirection:"column"}}>
-          <WorkDataTable datas={intervalsData}/>
-          </div>
+        {(checkForShow())?(<div>
+            <DynamicHistogram2 datas={valuesToValueAndLabels(histogramValues)}/>
+            <div style={{display:'flex', justifyContent:'center',alignItems:'center',flexDirection:"column"}}>
+            <WorkDataTable datas={intervalsData}/>
+            </div>
+            </div>):null
+        }
+        
         </div>
     );
     
