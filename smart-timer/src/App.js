3d13c642e53d4a680a1adb0fd4f2e7f2c37d65ab
sezgink <sezgink@users.noreply.github.com>
2022-12-14 18:37:23 +0300

    Converted to hashrouter from browser router

--- smart-timer/src/App.js
+++ smart-timer/src/App.js
@@ -2,7 +2,8 @@
 import {useEffect, useState} from 'react';
 import Timer from './Timer';
 import Navbar from './Navbar';
-import { BrowserRouter as Router, Routes, Route}
+// importk { BrowserRouter as Router, Routes, Route}
+import {  Routes, Route}
     from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import SignupFrom2 from './signup2';
@@ -21,17 +22,18 @@ function App() {
 
   return (
     
-    <Router>
+    <>
     <Navbar userState={userState} onSignout={()=>onSignout(setUserState)}  />
     <Routes>
-        <Route exact path='/' element={userState.isSigned? <TimerForUser userState={userState}/>   :<Timer userState={userState} />} />
+        
         <Route path='/timer' element={userState.isSigned? <TimerForUser userState={userState}/>   :<Timer userState={userState} />} />
         <Route path='/login' element={<LoginForm setUserState={setUserState} />}>
         </Route>
         <Route path='/signup' element={<SignupFrom2 setUserState={setUserState} />} />
         <Route path='/workchart' element={<WorkChart userState={userState} />} />
+        <Route exact path='/' element={userState.isSigned? <TimerForUser userState={userState}/>   :<Timer userState={userState} />} />
     </Routes>
-    </Router>
+    </>
   );
 }
 
--- smart-timer/src/index.js
+++ smart-timer/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Timer.css'
+import {HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
